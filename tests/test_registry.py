9500b2cc18237011d9309998d6703fb7bf975aee
Rémy HUBSCHER <hubscher.remy@gmail.com>
2024-04-17 14:18:57 +0200

    Add a test to validate CZ encoding

--- tests/test_registry.py
+++ tests/test_registry.py
@@ -5,3 +5,9 @@ from schwifty import registry
 def test_validate_bics():
     for bic in (bank["bic"] for bank in registry.get("bank") if bank["bic"]):
         BIC(bic, allow_invalid=False)
+
+
+def test_validate_cz_encoding():
+    assert "Komerční banka, a.s.", "Československá obchodní banka, a. s." in [
+        bank["name"] for bank in registry.get("bank") if bank["country_code"] == "CZ"
+    ]
