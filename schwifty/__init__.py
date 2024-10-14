5c91df5dc1fe691d889d3f752e92c09eac0a9481
Martin Domke <mail@martindomke.net>
2024-10-14 11:12:57 +0200

    feat: Remove conditional import of importlib_metadata
    All supported Python versions come with the importlib.metadata package
    and the required APIs in the standard library.

--- schwifty/__init__.py
+++ schwifty/__init__.py
@@ -1,7 +1,4 @@
-try:
-    from importlib.metadata import version
-except ImportError:
-    from importlib_metadata import version  # type: ignore
+from importlib.metadata import version
 
 from schwifty.bban import BBAN
 from schwifty.bic import BIC
